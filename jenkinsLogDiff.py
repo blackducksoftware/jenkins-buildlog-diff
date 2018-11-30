464abf2f6717875404c66f4e6323a50e37c88f64
Vasiliy Sharapov <vasiliys@gmail.com>
2018-11-30 13:05:59 -0500

    Initial commit

---
+++ jenkinsLogDiff.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import requests # pip install requests
+import pprint
+import subprocess
+pp = pprint.pprint
+import json
+import jenkinsLogDiffConfig as conf
+
+# Turn off https warnings & verification
+if conf.ignoreHttpsWarnings:
+  from requests.packages.urllib3.exceptions import InsecureRequestWarning
+  requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+httpsVerification=conf.httpsVerification
+# Import conf variables
+jenkinsUrl=conf.jenkinsUrl
+jenkinsJob=conf.jenkinsJob
+diffTool=conf.diffTool
+diffOptArgs=conf.diffOptArgs
+
+# Jenkins protects against CSRF so this crumb has to be passed with all requests
+crumbResponse = json.loads(
+  requests.get(
+    jenkinsUrl + "/crumbIssuer/api/json", 
+    verify=httpsVerification
+  ).text
+)
+headers = {crumbResponse['crumbRequestField'] : crumbResponse['crumb']}
+
+
+def ask(q):
+  r = ''
+  while r not in {'y', 'n'}:
+    r = input(q + " (y/n)").lower().strip()
+    if len(r): r = r[0]
+  return r == 'y'
+
+def getBuilds():
+  builds = json.loads(
+    requests.get(
+      jenkinsUrl + jenkinsJob + '/api/json?tree=builds[number]',
+      headers=headers,
+      verify=httpsVerification
+    ).text
+  )['builds']
+  print("Found " +str(len(builds)) +" builds")
+  return builds
+
+allBuilds = getBuilds()
+
+def findLatestFailure():
+  builds = allBuilds
+  # Yeah, I know there's a more correct way to do this
+  latestNum = str(builds[0]['number'])
+  print("Latest build: " + str(latestNum))
+  build = getBuildByNumber(latestNum)
+  while not build['result'] == "FAILURE":
+    build = getBuildByNumber(build['previousBuild']['number'])
+  if build == None:
+    print("Couldn't find any failed builds, just gonna pretend there's something wrong with the latest")
+    build=getBuildByNumber(latestNum)
+  return build
+
+def getBuildByNumber(num):
+  build = json.loads(
+    requests.get(
+      jenkinsUrl + jenkinsJob + str(num) + '/api/json?tree=number,result,previousBuild[number]',
+      headers=headers,
+      verify=httpsVerification
+    ).text
+  )
+  return build
+
+def findFirstPassAfterBuild(build):
+  start = build['previousBuild']['number']
+  print("Looking for passes starting at build: " + str(start))
+  build = getBuildByNumber(start)
+  while build['result'] == "FAILURE":
+    build = getBuildByNumber(build['previousBuild']['number'])
+  if build == None:
+    print("Couldn't find any passing builds, just gonna pretend there's nothing wrong with " + str(start))
+    build=getBuildByNumber(start)
+  return build
+
+def diffBuilds(buildNumTuple):
+  fileNameList = []
+  for buildNum in buildNumTuple:
+    fn = '/tmp/jenkinsBuildDiff_build_' + str(buildNum)
+    fileNameList.append(fn)
+    f = open(fn, 'w')
+    f.write(
+      requests.get(
+        jenkinsUrl + jenkinsJob + str(buildNum) +'/consoleText',
+        headers=headers,
+        verify=httpsVerification
+      ).text
+    )
+    f.close()
+  subprocess.run([diffTool] + diffOptArgs + fileNameList)
+  return
+
+latestFailure=findLatestFailure()
+print("Latest failure: " + str(latestFailure['number']))
+firstPassAfterFail=findFirstPassAfterBuild(latestFailure)
+print("First pass after " + str(latestFailure['number']) + " : " + str(firstPassAfterFail['number']))
+buildsToDiff = (latestFailure['number'], firstPassAfterFail['number'])
+if not ask("Do you want to diff " + str(buildsToDiff) + " ?"):
+  print("Alright... I see... you think you're so smart? Fine, you tell me what to diff. Here are all the builds Einstein:")
+  print(list(map(lambda x: x['number'], allBuilds)))
+  buildsToDiff = tuple(map(lambda x: int(input("Enter the " + x + " build number: ")), ["first", "second"]))
+diffBuilds(buildsToDiff)
+
---
+++ jenkinsLogDiffConfig.py
@@ -0,0 +1,13 @@
+# Look these over
+
+httpsVerification=False
+httpsWarnings=False
+jenkinsUrl="https://eng-jenkins-dev01.dc1.lan/"
+jenkinsJob="/job/Hub/job/Production/job/Master/job/Hub%20-%20BDS%20Operator%20Comp%20Test/"
+
+#### This script will run:
+# `$diffTool $diffOptArgs[0] $diffOptArgs[1] ... some file names'
+diffTool="vimdiff"
+diffOptArgs=[]
+
+# Protip: if you're viewing this file in less, hit 'v' to edit it with you $VISUAL or $EDITOR or `vi'
